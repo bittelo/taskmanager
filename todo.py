f9315463161940839a2370e342b6090f33e1c94a
bittelo <pedrohanza432@gmail.com>
2025-01-04 14:35:43 -0300

    Rascunho

---
+++ todo.py
@@ -0,0 +1,24 @@
+import flet as ft 
+
+class ToDo:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.page.bgcolor = ft.colors.WHITE
+        self.page.window_width = 450
+        self.page.window_height = 450
+        self.page.window_resizable = False
+        self.page.window_always_on_top = True
+        self.page.title = 'Gerenciador'
+        self.main_page()
+
+    def main_page(self):
+        input_task = ft.TextField(hint_text='Digite sua tarefa aqui')
+        input_bar = ft.Row(
+            controls=[
+                input_task,
+                ft.FloatingActionButton(icon=ft.icons.ADD)
+            ]
+        )
+        self.page.add(input_bar)
+
+ft.app(target=ToDo)
